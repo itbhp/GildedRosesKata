3c8728935a1b10b6eec6d106037c481826c8f33a
Paolo Sciarra <itbhps@gmail.com>
2023-01-22 13:20:04 +0100

    fix build

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,8 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 class GildedRoseTest {
 
